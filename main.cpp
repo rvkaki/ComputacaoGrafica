a9461d33e404eb495ed1209c252c26e02d51432a
Sacitob <SacitobJose@github.com>
2019-02-23 14:16:27 +0000

    Figuras prontas

--- main.cpp
+++ main.cpp
@@ -4,10 +4,15 @@
 #include <GL/glut.h>
 #endif
 
-#define _USE_MATH_DEFINES
-#include <math.h>
 #include <iostream>
 #include <fstream>
+#include <stdio.h>
+#include <string.h>
+
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+using namespace std;
 
 ofstream f;
 
@@ -15,6 +20,10 @@ GLdouble dist = 8, beta = M_PI_4, alpha = M_PI_4;
 
 int ac;
 char **av = NULL;  
+
+void drawVertex(float x, float y, float z) {
+	f << x << " " << y << " " << z << "\n";
+}
 
 void usage(char *shape){
 	if(strcmp(shape, "sphere") == 0)
@@ -51,50 +60,24 @@ void changeSize(int w, int h) {
 	glMatrixMode(GL_MODELVIEW);
 }
 
-
-void drawCone(float radius, float height, int slices) {
-	// put code to draw cylinder in here
-	GLdouble alphaDelta = 2 * M_PI / slices;
-
-	glBegin(GL_TRIANGLES);
-
-
-	for (GLdouble i = 0; i < slices; i++) {
-		// Festa dos tri�ngulos com os bicos para cima
-		glColor3f(1, 1, 0);
-		glVertex3f(radius*sin(i*alphaDelta), 0, radius*cos(i*alphaDelta));
-		glVertex3f(radius*sin((i + 1)*alphaDelta), 0, radius*cos((i + 1)*alphaDelta));
-		glVertex3f(0, height, 0);
-
-		// Circunfer�ncia de baixo
-		glColor3f(rand() / (float)RAND_MAX, rand() / (float)RAND_MAX,rand()/(float)RAND_MAX);
-		glVertex3f(0, 0, 0);
-		glVertex3f(radius*sin(i*alphaDelta), 0, radius*cos(i*alphaDelta));
-		glVertex3f(radius*sin((i - 1)*alphaDelta), 0, radius*cos((i - 1)*alphaDelta));
-
-	}
-
-	glEnd();
-}
-
 void drawPlane(int x, int z) {
     glBegin(GL_TRIANGLES);
         glColor3f(rand() / (float)RAND_MAX, rand() / (float)RAND_MAX,rand()/(float)RAND_MAX);
-        glVertex3f(0, 0, z / 2);
-        glVertex3f(x / 2, 0, 0);
-        glVertex3f(0, 0, -z / 2);
+        drawVertex(0, 0, z / 2);
+        drawVertex(x / 2, 0, 0);
+        drawVertex(0, 0, -z / 2);
 
-        glVertex3f(0,0,-z/2);
-        glVertex3f(-x/2,0,0);
-        glVertex3f(0,0,z/2);
+        drawVertex(0,0,-z/2);
+        drawVertex(-x/2,0,0);
+        drawVertex(0,0,z/2);
 
-		glVertex3f(0, 0, -z / 2);
-        glVertex3f(x / 2, 0, 0);
-        glVertex3f(0, 0, z / 2);
+		drawVertex(0, 0, -z / 2);
+        drawVertex(x / 2, 0, 0);
+        drawVertex(0, 0, z / 2);
         
-        glVertex3f(0,0,z/2);
-        glVertex3f(-x/2,0,0);
-        glVertex3f(0,0,-z/2);
+        drawVertex(0,0,z/2);
+        drawVertex(-x/2,0,0);
+        drawVertex(0,0,-z/2);
         glEnd();
 
 }
@@ -104,62 +87,134 @@ void drawBox(int x, int y, int z, int nr = 2){
 		glColor3f(rand() / (float)RAND_MAX, rand() / (float)RAND_MAX,rand()/(float)RAND_MAX);
 		
 		//Face cima
-		glVertex3f(x/2, y/2, z/2);
-		glVertex3f(x/2, y/2, -z/2);
-		glVertex3f(-x/2, y/2, -z/2);
+		drawVertex(x/2, y/2, z/2);
+		drawVertex(x/2, y/2, -z/2);
+		drawVertex(-x/2, y/2, -z/2);
 
-		glVertex3f(-x/2, y/2, -z/2);
-		glVertex3f(-x/2, y/2, z/2);
-		glVertex3f(x/2, y/2, z/2);
+		drawVertex(-x/2, y/2, -z/2);
+		drawVertex(-x/2, y/2, z/2);
+		drawVertex(x/2, y/2, z/2);
 
 		//Face baixo
-		glVertex3f(x/2, -y/2, -z/2);
-		glVertex3f(x/2, -y/2, z/2);
-		glVertex3f(-x/2, -y/2, z/2);
+		drawVertex(x/2, -y/2, -z/2);
+		drawVertex(x/2, -y/2, z/2);
+		drawVertex(-x/2, -y/2, z/2);
 
-		glVertex3f(-x/2, -y/2, z/2);
-		glVertex3f(-x/2, -y/2, -z/2);
-		glVertex3f(x/2, -y/2, -z/2);
+		drawVertex(-x/2, -y/2, z/2);
+		drawVertex(-x/2, -y/2, -z/2);
+		drawVertex(x/2, -y/2, -z/2);
 
 		//Face lateral 1
-		glVertex3f(x/2, -y/2, z/2);
-		glVertex3f(x/2, y/2, z/2);
-		glVertex3f(-x/2, y/2, z/2);
+		drawVertex(x/2, -y/2, z/2);
+		drawVertex(x/2, y/2, z/2);
+		drawVertex(-x/2, y/2, z/2);
 
-		glVertex3f(-x/2, y/2, z/2);
-		glVertex3f(-x/2, -y/2, z/2);
-		glVertex3f(x/2, -y/2, z/2);
+		drawVertex(-x/2, y/2, z/2);
+		drawVertex(-x/2, -y/2, z/2);
+		drawVertex(x/2, -y/2, z/2);
 
 		//Face lateral 2
-		glVertex3f(-x/2, -y/2, z/2);
-		glVertex3f(-x/2, y/2, z/2);
-		glVertex3f(-x/2, y/2, -z/2);
+		drawVertex(-x/2, -y/2, z/2);
+		drawVertex(-x/2, y/2, z/2);
+		drawVertex(-x/2, y/2, -z/2);
 
-		glVertex3f(-x/2, y/2, -z/2);
-		glVertex3f(-x/2, -y/2, -z/2);
-		glVertex3f(-x/2, -y/2, z/2);
+		drawVertex(-x/2, y/2, -z/2);
+		drawVertex(-x/2, -y/2, -z/2);
+		drawVertex(-x/2, -y/2, z/2);
 		
 		//Face lateral 3
-		glVertex3f(x/2, -y/2, -z/2);
-		glVertex3f(x/2, y/2, -z/2);
-		glVertex3f(x/2, y/2, z/2);
+		drawVertex(x/2, -y/2, -z/2);
+		drawVertex(x/2, y/2, -z/2);
+		drawVertex(x/2, y/2, z/2);
 
-		glVertex3f(x/2, y/2, z/2);
-		glVertex3f(x/2, -y/2, z/2);
-		glVertex3f(x/2, -y/2, -z/2);
+		drawVertex(x/2, y/2, z/2);
+		drawVertex(x/2, -y/2, z/2);
+		drawVertex(x/2, -y/2, -z/2);
 
 		//Face lateral 4
-		glVertex3f(-x/2, -y/2, -z/2);
-		glVertex3f(-x/2, y/2, -z/2);
-		glVertex3f(x/2, y/2, -z/2);
+		drawVertex(-x/2, -y/2, -z/2);
+		drawVertex(-x/2, y/2, -z/2);
+		drawVertex(x/2, y/2, -z/2);
 
-		glVertex3f(x/2, y/2, -z/2);
-		glVertex3f(x/2, -y/2, -z/2);
-		glVertex3f(-x/2, -y/2, -z/2);
+		drawVertex(x/2, y/2, -z/2);
+		drawVertex(x/2, -y/2, -z/2);
+		drawVertex(-x/2, -y/2, -z/2);
 
 		glEnd();
 }
 
+void drawCone(float radius, float height, int slices, int stacks) {
+	double sl = 2 * M_PI / slices;
+	float r = radius/((float)stacks);
+	float h = height/((float)stacks);
+
+	for (int i = 0; i < slices; i++) {
+		drawVertex(0,0,0);
+		drawVertex(radius * sin(i+1) * sl, 0, radius * cos(i+1) * sl);
+		drawVertex(radius * sin(i*sl), 0, radius * cos(i*sl));
+	}
+
+	float nr = radius - r;
+	for (int i = 0; i < stacks; i++) {
+		height = i * h;
+		float nh = (i+1) * h;
+
+		for(float j = 0.5*(i%2); j < slices + 0.5*(i%2); j++)
+		{
+			// Triangulas virados para cima
+			drawVertex(radius * sin(i*sl), height, radius * cos(i*sl));
+			drawVertex(radius * sin((i+1)*sl), height, radius * cos((i+1)*sl));
+			drawVertex(nr * sin((i+0.5)*sl), nh, nr * cos((i+0.5)*sl));
+
+			i += 0.5;
+			// Triangulos virados para baixo
+			drawVertex(nr * sin((i+1)*sl), nh, nr * cos((i+1)*sl));
+			drawVertex(nr*sin((i+1)*sl), nh, nr*cos((i+1)*sl));
+			drawVertex(radius * sin((i+0.5)*sl), height, radius * cos((i+0.5)*sl));
+			i -= 0.5;
+		}
+		radius = nr;
+		nr -= r;
+	}
+}
+
+void drawSphere(int radius, int slices, int stacks) {
+	double sl = 2 * M_PI / slices;
+	double beta = M_PI_2 / (stacks / 2);
+	float nr, h, nh;
+
+	float r = radius;
+	for(int i = 0; i < ((float)stacks)/2; i++) {
+		r = nr;
+		nr = radius * cos(beta*(i+1));
+		h = radius * sin(beta*i);
+		nh = radius * sin(beta*(i+1));
+
+		for(int j = 0.5 * (i%2); j < slices + 0.5*(j%2); i++) {
+			//Triangulos voltados para cima - metade superior
+			drawVertex(r * sin(i*sl), h, r * cos(i*sl));
+			drawVertex(r * sin((i+1)*sl), h, r * cos((i+1)*sl));
+			drawVertex(nr * sin((i+0.5)*sl), nh, nr * cos((i+0.5)*sl));
+		
+			// Triangulos voltados para cima - metade inferior
+			drawVertex(r*sin(i*sl), -h, r*cos(i*sl));
+			drawVertex(nr*sin((i+0.5)*sl), -nh, nr*cos((i+0.5)*sl));
+			drawVertex(r*sin((i+1)*sl), -h, r*cos((i+1)*sl));
+
+			i += 0.5;
+			// Triangulos voltados para baixo - metade superior
+			drawVertex(nr * sin((i+1)*sl), nh, nr * cos((i+1)*sl));
+			drawVertex(nr * sin(i*sl), nh, nr * cos(i*sl));
+			drawVertex(r * sin((i+0.5)*sl), h, r * cos((i+0.5)*sl));
+
+			// Triangulos voltados para baixo - metade inferior
+			drawVertex(nr * sin((i+1)*sl), -nh, nr * cos((i+1)*sl));
+			drawVertex(r * sin((i+0.5)*sl), -h, r*cos((i+0.5)*sl));
+			drawVertex(nr * sin(i*sl), -nh, nr*cos(i*sl));
+			i -= 0.5;
+		}
+	}
+}
 
 void renderScene() {
 	int badUsage = 0;
@@ -248,8 +303,8 @@ void processSpecialKeys(int key, int xx, int yy) {
 }
 
 int main(int argc, char **argv) {
-
 	int x,y,z;
+	int radius, height, stacks, slices;
 
 	if (argc < 5) {
 		printf("É necessário indicar o ficheiro de destino e a forma geométrica a ser desenhada\n");
@@ -260,7 +315,7 @@ int main(int argc, char **argv) {
 
 	switch (string(argv[1]))
 	{
-		case 'plane':
+		case "plane":
 			if(argc != 5) {
 				printf("Número incorreto de argumentos\n");
 				usage("plane");
@@ -272,7 +327,7 @@ int main(int argc, char **argv) {
 			drawPlane(x,z); 
 			break;
 
-		case 'box':
+		case "box":
 			if(argc != 7 || argc != 6) {
 				printf("Número incorreto de argumentos\n");
 				usage("box");
@@ -283,10 +338,42 @@ int main(int argc, char **argv) {
 			y = atoi(argv[3]);
 			z = atoi(argv[4]);
 			drawBox(x,y,z);
+			break;
+
+		case "cone":
+			if(argc != 7) {
+				printf("Número incorreto de argumentos\n");
+				usage("cone");
+				exit(EXIT_FAILURE);
+			}
+
+			radius = atoi(argv[2]);
+			height = atoi(argv[3]);
+			slices = atoi(argv[4]);
+			stacks = atoi(argv[5]);
+			drawCone(radius, height, slices, stacks);
+			break;
+
+		case "sphere":
+			if(argc != 6) {
+				printf("Número incorreto de argumentos\n");
+				usage("sphere");
+				exit(EXIT_FAILURE);
+			}
+
+			radius = atoi(argv[2]);
+			slices = atoi(argv[3]);
+			stacks = atoi(argv[4]);
+			drawSphere(radius, slices, stacks);
+			break;
 	
 		default:
+			printf("Figura não suportada\n");
+			exit(EXIT_FAILURE);
 			break;
 	}
+
+	f.close();
 
 	// init GLUT and the window
 	glutInit(&argc, argv);
